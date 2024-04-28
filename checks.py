ff389b39e8211dca7dc57155bc67a89edc4858f4
Alan Bidart <alanbidart@gmail.com>
2024-04-28 16:29:01 -0400

    add text to assertions

--- checks.py
+++ checks.py
@@ -2,5 +2,5 @@ import numpy as np
 
 
 def check_valid_lattice(lattice):
-    assert np.all((lattice == 0) | (lattice == 1))
-    assert np.sum(lattice[0]) == np.sum(lattice[1])
+    assert np.all((lattice == 0) | (lattice == 1)), "All sites should 0 or 1"
+    assert np.sum(lattice[0]) == np.sum(lattice[1]), "The number of up and down spins is different"
